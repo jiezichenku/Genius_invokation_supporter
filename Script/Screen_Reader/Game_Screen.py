4a6ca8050bc57b56dd07973206a3f50c96b015f0
xxxq <jiezichenku@gmail.com>
2023-06-26 00:43:08 +0800

    Flann_sift

--- Script/Screen_Reader/Game_Screen.py
+++ Script/Screen_Reader/Game_Screen.py
@@ -19,10 +19,10 @@ class Game_Screen:
                                                       width, height))
         return self.image
 
-    def check_card_exist(self, card, threshold):
+    def check_card_exist(self, screen, card, threshold):
         # 图片文件夹路径和游戏画面截图路径
         graphic = card  # 卡牌图片文件夹路径
-        target = cv2.cvtColor(np.array(self.image), cv2.COLOR_RGB2BGR)
+        # target = cv2.cvtColor(np.array(self.image), cv2.COLOR_RGB2BGR)
         # 初始化FLANN匹配器
         FLANN_INDEX_KDTREE = 1
         index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
@@ -31,6 +31,7 @@ class Game_Screen:
 
         # 遍历图片文件夹中的所有图片
         img = cv2.imread(graphic, 0)
+        target = cv2.imread(screen, 0)
 
         # 检测和计算关键点和描述符
         sift = cv2.SIFT_create()
@@ -46,57 +47,49 @@ class Game_Screen:
             if m.distance < threshold * n.distance:
                 good_matches.append(m)
 
+        # 获取模板在屏幕截图中的位置
+        locations = []
+        for match in good_matches:
+            x, y = kp_target[match.trainIdx].pt
+            locations.append((int(x), int(y)))
+            cv2.circle(target, (int(x), int(y)), 5, (0, 255, 0), -1)
 
-        if len(good_matches) > 10:
-            # 提取匹配点的坐标
-            src_pts = np.float32([kp_img[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
-            dst_pts = np.float32([kp_target[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
-            print("src_pts: %s \ndst_pts: %s" % (src_pts, dst_pts))
-
-            # 使用RANSAC算法计算透视变换矩阵
-            M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-
-            # 获取图片的宽度和高度
-            img_width, img_height = img.shape[1], img.shape[0]
-
-            # 计算图像四个角经透视变换后的坐标
-            pts = np.float32([[0, 0], [0, img_height], [img_width, img_height], [img_width, 0]]).reshape(-1, 1, 2)
-            dst = cv2.perspectiveTransform(pts, M)
-            return dst
-
-        return None
-
-    def check_screen_status(self, template):
-        # 读取游戏界面截图
-        game_screen = cv2.cvtColor(np.array(self.image), cv2.COLOR_RGB2BGR)
-        # 读取模板图像
-        template_image = cv2.imread(template)
-
-        # 使用模板匹配算法
-        result = cv2.matchTemplate(game_screen, template_image, cv2.TM_CCOEFF_NORMED)
-
-        # 设置匹配阈值
-        threshold = 0.8
-        # 获取匹配结果中匹配位置的坐标
-        locations = np.where(result >= threshold)
-        # 绘制矩形框标记匹配位置
-        for pt in zip(*locations[::-1]):
-            cv2.rectangle(game_screen, pt, (pt[0] + template_image.shape[1], pt[1] + template_image.shape[0]),
-                          (0, 255, 0), 2)
-
-        # 显示带有标记的游戏界面
-        cv2.imshow('Game Screen', game_screen)
+        # 显示屏幕截图并标记匹配位置
+        cv2.imshow('Screen', target)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
+        # 获取模板的数量
+        num_templates = len(locations)
+        return num_templates, locations
+
+
+        # if len(good_matches) > 10:
+        #     # 提取匹配点的坐标
+        #     src_pts = np.float32([kp_img[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+        #     dst_pts = np.float32([kp_target[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+        #     print("src_pts: %s \ndst_pts: %s" % (src_pts, dst_pts))
+        #
+        #     # 使用RANSAC算法计算透视变换矩阵
+        #     M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+        #
+        #     # 获取图片的宽度和高度
+        #     img_width, img_height = img.shape[1], img.shape[0]
+        #
+        #     # 计算图像四个角经透视变换后的坐标
+        #     pts = np.float32([[0, 0], [0, img_height], [img_width, img_height], [img_width, 0]]).reshape(-1, 1, 2)
+        #     dst = cv2.perspectiveTransform(pts, M)
+        #     return dst
+        #
+        # return None
 
 if __name__ == '__main__':
     screen_reader = Game_Screen()
-    screen_reader.get_game_screenshot()
-    time.sleep(1)
+    # screen_reader.get_game_screenshot()
+    # time.sleep(1)
+    screen = "E:\GitHub\GenshinCard\Graphic\Game\deck_card.png"
     path = "E:\GitHub\GenshinCard\Graphic\Event\I_Havent_Lost_Yet.png"
-    num, pos = screen_reader.check_card_exist(path, 0.9)
-    print("num: %s, pos:\n%s" % (num, pos))
-
-
-
+    for i in range(10):
+        t = 0.1*i
+        num, pos = screen_reader.check_card_exist(screen, path, t)
+        print("threshold: %s, num: %s, pos: %s" % (t, num, pos))
